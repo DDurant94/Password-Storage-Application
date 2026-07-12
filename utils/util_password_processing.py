8113ed259c6d9f320deedf2fc2083a96c1b69709
Daniel Durant <dannyjdurant@gmail.com>
2026-07-11 21:41:40 -0500

    Abstration: Adding abstraction and better encapsulation for password processing

---
+++ utils/util_password_processing.py
@@ -0,0 +1,8 @@
+# This is going to be for encapsulation of code for password processing encrypting and decrypting
+
+# To Do
+# - Make a single entry point for password processes
+# - Pass an observer to watch for the process
+# - Give the information that we either want to add or update. Generating a new salt and blob for the password
+# - Pass the only want is need to the function to see and nothing more. Allowing for other functions to handle only what the need.
+# - Make sure that if the anything fails along the way that it doesn't insert the information causing the system to not normalize
